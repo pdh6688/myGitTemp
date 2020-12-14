2b296bd5ec6a8a6be9de5c2089024bb12db5c01e
pdh6868 <2197627117@qq.com>
2020-12-14 15:51:13 +0800

    分支添加

--- HelloGit/src/main/java/com/pdh/git/HelloGit.java
+++ HelloGit/src/main/java/com/pdh/git/HelloGit.java
@@ -8,5 +8,7 @@ public class HelloGit {
     public static void main(String[] args) {
         System.out.println("Hello Git");
         System.out.println("创建分支");
+        System.out.println("主干添加");
+        System.out.println("分支添加");
     }
 }
